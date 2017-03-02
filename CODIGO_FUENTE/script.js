e2c3d6b0832848a395ce8aee8a6ec2d96cd22273
2013034348 Jose Mario Naranjo Leiva IC <jonaranjo@ec.tec.ac.cr>
2017-03-02 17:47:23 -0600

    source code

---
+++ CODIGO_FUENTE/script.js
@@ -0,0 +1,22 @@
+
+var liceoRobleApp = angular.module('liceoRobleApp', ['ngRoute']);
+
+
+// configure our routes
+liceoRobleApp.config(function($routeProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html'
+		})
+
+		.when('/galeria', {
+			templateUrl : 'pages/galeria.html'
+		})
+
+		.when('/historia', {
+			templateUrl : 'pages/historia.html'
+		});
+	
+});
