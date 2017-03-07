3f58435a29613bd3ddbf88c7bbf88ff4a8759e96
2013034348 Jose Mario Naranjo Leiva IC <jonaranjo@ec.tec.ac.cr>
2017-03-07 12:59:57 -0600

    Noticias en la página principal

--- CODIGO_FUENTE/script.js
+++ CODIGO_FUENTE/script.js
@@ -7,7 +7,8 @@ liceoRobleApp.config(function($routeProvider) {
 	$routeProvider
 
 		.when('/', {
-			templateUrl : 'pages/home.html'
+			templateUrl : 'pages/home.html',
+			controller : 'homeController'
 		})
 
 		.when('/galeria', {
@@ -39,4 +40,13 @@ liceoRobleApp.controller('misionvisionController', function($scope) {
 	$scope.content_mision = "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 	$scope.content_vision = "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 
+});
+
+
+liceoRobleApp.controller('homeController', function($scope){
+
+	$scope.news = [
+					{title:'Noticia 1', date : '25 de marzo', content : 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.'},
+					{title:'Noticia 2', date : '10 de enero de 3010', content : 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat...'}
+				];
 });
