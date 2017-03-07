f9d0699379985ddaff612340fc32c26ac6a41189
2013034348 Jose Mario Naranjo Leiva IC <jonaranjo@ec.tec.ac.cr>
2017-03-07 10:57:00 -0600

    Controlador de la página de misión y visión

--- CODIGO_FUENTE/script.js
+++ CODIGO_FUENTE/script.js
@@ -19,8 +19,9 @@ liceoRobleApp.config(function($routeProvider) {
 		})
 
 		.when('/misionvision', {
-			templateUrl : 'pages/misionvision.html'
-			})
+			templateUrl : 'pages/misionvision.html' ,
+			controller : 'misionvisionController'
+		})
 		.when('/contacto', {
 			templateUrl : 'pages/contacto.html'
 
@@ -29,8 +30,13 @@ liceoRobleApp.config(function($routeProvider) {
 			templateUrl : 'pages/servicio.html'
 
 		})
-		
-		
 		;
-	
+});
+
+
+liceoRobleApp.controller('misionvisionController', function($scope) {
+
+	$scope.content_mision = "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+	$scope.content_vision = "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+
 });
